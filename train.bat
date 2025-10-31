879ddd30c1b34a7cdbcef2d0fd4c3cdd507b015d
wudi <wudi@dexforce.com>
2025-10-31 14:11:55 +0800

    get_gait_phase_reward函数最后的clamp阈值从0.1加到0.2再加到0.4,让抬腿更高。每加一次训练3000 episode。chechpoint：2025-10-31_11-36-00

--- train.bat
+++ train.bat
@@ -1,2 +1,2 @@
 rem python scripts\rsl_rl\train.py --task=Template-Direct-Pm01-Walk-Direct-v0 --max_iterations=30000  > log.txt
-python scripts\rsl_rl\train.py --task=Template-Direct-Pm01-Walk-Direct-v0 --max_iterations=30000 --resume --load_run=2025-10-31_00-01-53 --headless > log.txt
+python scripts\rsl_rl\train.py --task=Template-Direct-Pm01-Walk-Direct-v0 --max_iterations=3000 --resume --load_run=2025-10-31_10-11-59 --headless > log.txt
